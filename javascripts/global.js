d3503710b25ba1c7bec2f4159abe3db2c65cbd7d
Callum Isitt <callum@callumisitt.co.uk>
2015-06-26 00:21:00 +0100

    colour changes and vimeo embed

--- javascripts/global.js
+++ javascripts/global.js
@@ -1,23 +1,21 @@
 $(document).ready(function(){
 
-  $("a[href^='http']").attr('target','_blank');
+	$("a[href^='http']").attr('target','_blank');
 
-  $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	    var $target = $(this.hash);
-	    $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-          var targetOffset = $target.offset().top - 50;
-      
-					$('html,body').animate({scrollTop: targetOffset}, 750);
-					if ($("h2.smart-reveal").hasClass("expanded")) {
-          	$("h2.smart-reveal").click();								
-					};
-
-					return false;
+	$('a[href*=#]').click(function() {
+		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+			var $target = $(this.hash);
+			$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+			if ($target.length) {
+				var targetOffset = $target.offset().top - 50;
+				$('html,body').animate({scrollTop: targetOffset}, 750);
+				if ($("h2.smart-reveal").hasClass("expanded")) {
+					$("h2.smart-reveal").click();
+				}
+				return false;
 			}
-  	}
-  });
+		}
+	});
 
 	// responsive primary menu
 	$("h2.smart-reveal").click(function(e) {
@@ -27,9 +25,10 @@ $(document).ready(function(){
 		} else {
 			$(this).addClass('expanded');
 			$(this).parent().addClass('open-responsive');
-		};
-
+		}
 		e.preventDefault();
 	});
+
+	$("#media").fitVids();
 
 });
